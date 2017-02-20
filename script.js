55d2649d14a931d50f1363e2b6fe2a6d91481bc2
Antoine <ant.hess2@gmail.com>
2017-02-21 00:38:31 +0100

    Adding CSS/HTML, Imgs and scripts

---
+++ script.js
@@ -0,0 +1,15 @@
+function showAbout(){
+  document.getElementById('main-1').style.display = 'none';
+  document.getElementById('main-2').style.display = 'inline';
+  document.getElementById('main-3').style.display = 'none';
+}
+function showMe(){
+  document.getElementById('main-1').style.display = 'inline';
+  document.getElementById('main-2').style.display = 'none';
+  document.getElementById('main-3').style.display = 'none';
+}
+function showSkills(){
+  document.getElementById('main-1').style.display = 'none';
+  document.getElementById('main-2').style.display = 'none';
+  document.getElementById('main-3').style.display = 'inline';
+}
