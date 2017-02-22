2df8b1b6022a721ea10c7380ead62ee51fdc53ba
Antoine <ant.hess2@gmail.com>
2017-02-22 08:21:56 +0100

    Ajout css et skills

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function showSkills(){
   document.getElementById('main-2').style.display = 'none';
   document.getElementById('main-3').style.display = 'inline';
 }
+function move() {
+  var elem = document.getElementById("myBar1");
+  var width = 20;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+      document.getElementById("demo").innerHTML = width * 1  + '%';
+    }
+  }
+}
